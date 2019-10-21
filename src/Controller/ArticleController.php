f9b920f4ce0e1f052973a66a49e843c43d038583
miguelidevelop <miguelidevelop@gmail.com>
2019-10-21 11:01:13 +0100

    route change slug

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,14 +1,31 @@
 <?php
 
 namespace App\Controller;
-
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController {
+
+    /**
+     * @Route("/")
+     */
 
     public function homepage() {
 
         return new Response('OMG! My first page already! Wooo!');
+    }
+
+    /**
+     * @Route("/noticias/{slug}")
+     */
+
+    public function show($slug) {
+
+        return new Response(sprintf(
+            'Futura página sobre las últimas noticias de: %s', 
+            $slug
+        ));
+
     }
 
 }
